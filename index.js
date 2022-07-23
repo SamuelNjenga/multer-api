68f3fcefebb96cea02e50a49b1f96d3804a62b5e
Samuel Njenga <snjenga704@gmail.com>
2022-07-23 16:01:16 +0300

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/upload", (req, res) => {
+  res.render("upload");
+});
+
+app.post("/upload", (req, res) => {
+  res.send("Image Uploaded");
+});
+
+app.listen(3001);
+console.log("3001 is the port");
