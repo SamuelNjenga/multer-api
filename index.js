9d8073abb154de87e135a5841dbe94ad5df7efd4
Samuel Njenga <snjenga704@gmail.com>
2022-07-23 16:39:14 +0300

    feat: add images directory

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "Images");
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
 
 app.set("view engine", "ejs");
 
@@ -7,7 +21,7 @@ app.get("/upload", (req, res) => {
   res.render("upload");
 });
 
-app.post("/upload", (req, res) => {
+app.post("/upload", upload.single("image"), (req, res) => {
   res.send("Image Uploaded");
 });
 
